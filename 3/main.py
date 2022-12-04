2a304facad00ca7a1a87726c734e43187ad78ea4
Jeremy Thompson <jeremythompson.918@gmail.com>
2022-12-04 12:26:00 +0000

    Solve day three, part two

--- 3/main.py
+++ 3/main.py
@@ -34,23 +34,35 @@ def find_shared(first, second):
     return letter
 
 
+def find_shared_badge(group: list):
+    # use only unique characters from each rucksack
+    unique_rucksacks = list(map(set, group))
+
+    # convert from set to string
+    str_rucksacks = list(map(lambda x: ''.join(x), unique_rucksacks))
+
+    # concat all rucksacks together
+    concatted = ''.join(str_rucksacks)
+
+    # get duplicate letter
+    counter = Counter(concatted)
+
+    # get shared character
+    [(letter, count)] = counter.most_common(1)
+    assert count == 3
+
+    return letter
+
+
 with open('input/input.txt') as f:
-    lines = f.readlines()
-
     sum = 0
+    group_lines = []
 
-    for line in lines:
-        line = line.strip()
-
-        length = len(line)
-        half = int(length / 2)
-
-        first = line[0:half]
-        second = line[half:]
-
-        shared = find_shared(first, second)
-        priority = get_priority(shared)
-
-        sum += priority
+    for line in f:
+        group_lines.append(line.strip())
+        if len(group_lines) >= 3:
+            badge = find_shared_badge(group_lines)
+            sum += get_priority(badge)
+            group_lines = []
 
     print(sum)
