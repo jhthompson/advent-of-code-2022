32639c6dd28dde3b708986be00b518a1c5beb814
Jeremy Thompson <jeremythompson.918@gmail.com>
2022-12-07 21:48:37 +0000

    Solve day six, part one

---
+++ 6/main.py
@@ -0,0 +1,27 @@
+
+
+from collections import Counter
+
+
+def find_first_unique_occurence(line: str, window_size: int):
+    window = ''
+
+    for index, char in enumerate(line):
+        window += char
+
+        if len(window) > window_size:
+            window = window[1:]
+
+        if len(window) == window_size:
+            freq = Counter(window)
+
+            if (len(freq) == len(window)):
+                return index + 1
+
+    return -1
+
+
+with open('input/input.txt') as f:
+    line = f.readline().strip()
+
+    print(find_first_unique_occurence(line, 4))
