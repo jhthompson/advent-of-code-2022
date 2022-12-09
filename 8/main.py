4f966782e732a04ba1a92b1e4dde8542a1787808
Jeremy Thompson <jeremythompson.918@gmail.com>
2022-12-09 23:43:27 +0000

    Solve day eight, part two

--- 8/main.py
+++ 8/main.py
@@ -13,73 +13,72 @@ def construct_2d_array(lines: list):
     return trees
 
 
-def compute_visible_trees(trees):
-    grid_size = len(trees[0])
-
-    num_visible_trees = 0
+def compute_best_scenic_score(trees):
+    best_scenic_score = 0
 
     for row_index, row in enumerate(trees):
         for column_index, column in enumerate(row):
-            if tree_is_visible(row_index, column_index, trees):
-                num_visible_trees += 1
+            scenic_score = compute_scenic_score(row_index, column_index, trees)
+            if scenic_score > best_scenic_score:
+                best_scenic_score = scenic_score
 
-    return num_visible_trees
+    return best_scenic_score
 
 
-def tree_is_visible(row_index, column_index, trees):
+def compute_scenic_score(row_index, column_index, trees) -> int:
     grid_size = len(trees[0])
-
-    # tree is on an edge
-    if row_index == 0 or column_index == 0 or row_index == grid_size - 1 or column_index == grid_size - 1:
-        return True
 
     tree = trees[row_index][column_index]
 
     # check left
     i = column_index - 1
-    visible_left = True
-    while i >= 0 and visible_left:
+    visible_left = 0
+    while i >= 0:
         neighbour = trees[row_index][i]
+        visible_left += 1
 
         if neighbour >= tree:
-            visible_left = False
+            break
 
         i -= 1
 
     # check up
     i = row_index - 1
-    visible_up = True
-    while i >= 0 and visible_up:
+    visible_up = 0
+    while i >= 0:
         neighbour = trees[i][column_index]
+        visible_up += 1
 
         if neighbour >= tree:
-            visible_up = False
+            break
 
         i -= 1
 
     # check right
     i = column_index + 1
-    visible_right = True
-    while i < grid_size and visible_right:
+    visible_right = 0
+    while i < grid_size:
         neighbour = trees[row_index][i]
+        visible_right += 1
 
         if neighbour >= tree:
-            visible_right = False
+            break
 
         i += 1
 
     # check down
     i = row_index + 1
-    visible_down = True
-    while i < grid_size and visible_down:
+    visible_down = 0
+    while i < grid_size:
         neighbour = trees[i][column_index]
+        visible_down += 1
 
         if neighbour >= tree:
-            visible_down = False
+            break
 
         i += 1
 
-    return visible_left or visible_up or visible_right or visible_down
+    return visible_left * visible_up * visible_right * visible_down
 
 
 with open('input/input.txt') as f:
@@ -89,6 +88,6 @@ with open('input/input.txt') as f:
     trees = construct_2d_array(lines)
 
     # compute number of visible trees
-    num_visible = compute_visible_trees(trees)
+    scenic_score = compute_best_scenic_score(trees)
 
-    print(num_visible)
+    print(scenic_score)
